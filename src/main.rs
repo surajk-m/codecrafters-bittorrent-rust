2ec74ea34d9d330047714f29476a50970ce0cd33
surajk-m <suraj410401@gmail.com>
2023-10-27 10:18:05 +0530

    decode bencoded integers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use serde_json;
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
+    if encoded_value.starts_with('i') {
+        // Integer case: i<number>e
+        let end_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..end_index];
+        let number = number_string.parse::<i64>().unwrap();
+        return serde_json::Value::Number(serde_json::Number::from(number));
+    } else if encoded_value.chars().next().unwrap().is_digit(10) {
+        // String case: <length>:<string>
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<usize>().unwrap();
@@ -19,16 +20,13 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     }
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         eprintln!("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
