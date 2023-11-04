88aa4fd1f1966460d45c8028e70ea9fa142590d9
surajk-m <suraj410401@gmail.com>
2023-11-05 04:17:48 +0530

    decode `bencoded` dictionaries

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use serde_json;
 use std::env;
+use serde_json::Map;
 
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
     match encoded_value.chars().next() {
         Some('i') => {
-            if let Some((n, rest)) =
-                encoded_value
-                    .split_at(1)
-                    .1
-                    .split_once('e')
-                    .and_then(|(digits, rest)| {
-                        let n = digits.parse::<i64>().ok()?;
-                        Some((n, rest))
-                    })
+            if let Some((n, rest)) = encoded_value
+                .split_at(1)
+                .1
+                .split_once('e')
+                .and_then(|(digits, rest)| {
+                    let n = digits.parse::<i64>().ok()?;
+                    Some((n, rest))
+                })
             {
                 return (n.into(), rest);
             }
@@ -27,6 +27,24 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
             }
             return (values.into(), &rest[1..]);
         }
+        Some('d') => {
+            let mut dict = serde_json::Map::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && rest.starts_with(|c| c != 'e') {
+                let (key, remainder) = decode_bencoded_value(rest);
+                let (value, remainder) = decode_bencoded_value(remainder);
+                if let serde_json::Value::String(key_str) = key {
+                    dict.insert(key_str, value);
+                } else {
+                    panic!("Dictionary keys must be strings.");
+                }
+                rest = remainder;
+            }
+            
+            let json_map: Map<String, serde_json::Value> = dict.into_iter().collect();
+            return (serde_json::Value::Object(json_map), &rest[1..]);
+        }
+
         Some('0'..='9') => {
             if let Some((len, rest)) = encoded_value.split_once(':') {
                 if let Ok(len) = len.parse::<usize>() {
@@ -37,8 +55,9 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
         _ => {}
     }
 
-    panic!("Unhandled encoded value: {}", encoded_value)
+    panic!("Unhandled encoded value: {}", encoded_value);
 }
+
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
