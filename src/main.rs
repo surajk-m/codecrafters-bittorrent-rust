8ab89d513fa1411df832df75c0b184a84bc6feec
surajk-m <suraj410401@gmail.com>
2023-11-04 05:31:19 +0530

    decode bencoded strings integers

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,18 @@ use serde_json;
 use std::env;
 
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.starts_with('i') {
-        // Integer case: i<number>e
-        let end_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..end_index];
-        let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(serde_json::Number::from(number));
-    } else if encoded_value.chars().next().unwrap().is_digit(10) {
-        // String case: <length>:<string>
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    if let Some(n) = encoded_value
+        .strip_prefix('i')
+        .and_then(|rest| rest.split_once('e'))
+        .and_then(|(digits, _)| digits.parse::<i64>().ok())
+    { 
+        return n.into();
+    } else if let Some((len, rest)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
     }
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 fn main() {
