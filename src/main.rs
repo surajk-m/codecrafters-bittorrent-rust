a35b0a06ed51b246f3857c08411e42713d30752e
surajk-m <suraj410401@gmail.com>
2023-11-17 06:50:33 +0530

    discover peers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use anyhow::Context;
+use bittorrent_starter_rust::torrent::{self, Torrent};
+use bittorrent_starter_rust::tracker::*;
 use clap::{Parser, Subcommand};
-use hashes::Hashes;
-use serde::{Deserialize, Serialize};
+use serde_bencode;
 use serde_json::{Map, Value};
-use sha1::{Digest, Sha1};
 use std::fs::read;
 use std::path::PathBuf;
+
+const DEFAULT_PORT: u16 = 6881;
+const DEFAULT_COMPACT: u8 = 1;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,35 +21,7 @@ struct Args {
 enum Command {
     Decode { value: String },
     Info { torrent: PathBuf },
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-struct Torrent {
-    announce: String,
-    info: Info,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-struct Info {
-    name: String,
-    #[serde(rename = "piece length")]
-    plength: usize,
-    pieces: Hashes,
-    #[serde(flatten)]
-    keys: Keys,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(untagged)]
-enum Keys {
-    SingleFile { length: usize },
-    MultiFile { files: Vec<File> },
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-struct File {
-    length: usize,
-    path: Vec<String>,
+    Peers { torrent: PathBuf },
 }
 
 fn decode_bencoded_value(encoded_value: &str) -> Result<(Value, &str), anyhow::Error> {
@@ -105,7 +80,17 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(Value, &str), anyhow::E
     }
 }
 
-fn main() -> anyhow::Result<()> {
+fn urlencode(t: &[u8; 20]) -> String {
+    let mut encoded = String::with_capacity(3 * t.len());
+    for &byte in t {
+        encoded.push('%');
+        encoded.push_str(&hex::encode(&[byte]));
+    }
+    encoded
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
@@ -121,16 +106,12 @@ fn main() -> anyhow::Result<()> {
             eprintln!("{t:?}");
             println!("Tracker URL: {}", t.announce);
 
-            match t.info.keys {
-                Keys::SingleFile { length } => println!("Length: {}", length),
+            match &t.info.keys {
+                torrent::Keys::SingleFile { length } => println!("Length: {}", length),
                 _ => todo!(),
             }
 
-            let info_encoded =
-                serde_bencode::to_bytes(&t.info).context("re-encode info section")?;
-            let mut hasher = Sha1::new();
-            hasher.update(&info_encoded);
-            let info_hash = hasher.finalize();
+            let info_hash = t.info_hash()?;
             println!("Info Hash: {}", hex::encode(&info_hash));
             println!("Piece Length: {}", t.info.plength);
             println!("Piece Hashes:");
@@ -138,59 +119,44 @@ fn main() -> anyhow::Result<()> {
                 println!("{}", hex::encode(&hash));
             }
         }
-    }
 
-    Ok(())
-}
+        Command::Peers { torrent } => {
+            let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
+            let t: Torrent =
+                serde_bencode::from_bytes(&dot_torrent).context("parse torrent file")?;
+            let length = match t.info.keys {
+                torrent::Keys::SingleFile { length } => length,
+                _ => {
+                    todo!();
+                }
+            };
 
-mod hashes {
-    use serde::de::{self, Deserialize, Deserializer, Visitor};
-    use serde::ser::{Serialize, SerializeMap, SerializeSeq, Serializer};
-    use std::fmt;
+            let info_hash = t.info_hash()?;
+            let request = TrackerRequest {
+                peer_id: String::from("00112233445566778899"),
+                port: DEFAULT_PORT,
+                uploaded: 0,
+                downloaded: 0,
+                left: length,
+                compact: DEFAULT_COMPACT,
+            };
 
-    #[derive(Debug, Clone)]
-    pub struct Hashes(pub Vec<[u8; 20]>);
-    struct HashesVisitor;
-
-    impl<'de> Visitor<'de> for HashesVisitor {
-        type Value = Hashes;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("a byte string whose length is a multiple of 20")
-        }
-
-        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            if v.len() % 20 != 0 {
-                return Err(E::custom(format!("length is {}", v.len())));
+            let url_params =
+                serde_urlencoded::to_string(&request).context("url-encode tracker parameters")?;
+            let tracker_url = format!(
+                "{}?{}&info_hash={}",
+                t.announce,
+                url_params,
+                &urlencode(&info_hash)
+            );
+            let response = reqwest::get(&tracker_url).await.context("query tracker")?;
+            let response = response.bytes().await.context("fetch tracker response")?;
+            let response: TrackerResponse =
+                serde_bencode::from_bytes(&response).context("parse tracker response")?;
+            for peer in &response.peers.0 {
+                println!("{}:{}", peer.ip(), peer.port());
             }
-            // TODO: use array_chunks when stable
-            Ok(Hashes(
-                v.chunks_exact(20)
-                    .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
-                    .collect(),
-            ))
         }
     }
-
-    impl<'de> Deserialize<'de> for Hashes {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            deserializer.deserialize_bytes(HashesVisitor)
-        }
-    }
-
-    impl Serialize for Hashes {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            let single_slice = self.0.concat();
-            serializer.serialize_bytes(&single_slice)
-        }
-    }
+    Ok(())
 }
